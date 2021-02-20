dcb4e0d68aff312b16c44dd8283a09ba64f7955f
WisnuCakraa <wisnucakraa@gmail.com>
2021-02-20 20:28:49 +0700

    [UPDATE] Routes and image configs

--- src/Configs/routes.js
+++ src/Configs/routes.js
@@ -2,6 +2,18 @@ const routes = {
   DASHBOARD() {
     return "/";
   },
+
+  FACEBOOK() {
+    return "https://www.facebook.com/wisnucakraa";
+  },
+
+  INSTAGRAM() {
+    return "https://www.instagram.com/wisnucakra_/";
+  },
+
+  LINKEDIN() {
+    return "https://www.linkedin.com/in/wisnu-cakra-basudewa-prasodjo-86b586199/";
+  },
 };
 
 export default routes;
